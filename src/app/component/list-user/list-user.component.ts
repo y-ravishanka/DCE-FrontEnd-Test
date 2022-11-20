5d0149d7e39eafbf80793d47d5174c6c831d3181
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-21 01:37:26 +0530

    create gerUser methos to connect to CREATE endpoint

--- src/app/component/list-user/list-user.component.ts
+++ src/app/component/list-user/list-user.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/User';
+import { AllUser } from 'src/app/AllUser';
 
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.css']
 })
+
 export class ListUserComponent {
 
+  txtArea?: string;
+
+  getUser(): void {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://reqres.in/api/users", true);
+    xhr.onload = function(){
+      console.log(xhr.response);
+      //alluser: AllUser = xhr.response;
+    };
+    xhr.send();
+    
+  }
 }
