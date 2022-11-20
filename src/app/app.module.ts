c9d2530a31c497d256a77c1420b1c73cb3b68df5
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-20 19:56:34 +0530

    create createUser component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './component/create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule
