c1edea44403131408b15303422d1b29cba04b88f
y-ravishanka <ravishanka.yasas@gmail.com>
2022-11-21 01:03:56 +0530

    create createUser method to use a post end point

--- src/app/component/create-user/create-user.component.ts
+++ src/app/component/create-user/create-user.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent {
+  txtName?: string;
+  txtJob?: string;
 
+  createUser(): void{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://reqbin.com/echo/post/json");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+      }};
+      
+      let data = {
+        "name" : this.txtName,
+        "job": this.txtJob
+      };
+
+      xhr.send(JSON.stringify(data));
+  }
 }
